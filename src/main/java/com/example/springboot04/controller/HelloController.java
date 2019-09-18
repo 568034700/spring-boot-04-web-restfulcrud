4a655aff66922dd33f1ef2647d7cf5150cefd3fe
caohong <568034700@qq.com>
2019-09-18 17:38:38 +0800

    哈哈哈

--- src/main/java/com/example/springboot04/controller/HelloController.java
+++ src/main/java/com/example/springboot04/controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.springboot04.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class HelloController {
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        map.put("hello", "<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan", "list", "wangwu"));
+        return "success";
     }
 }
